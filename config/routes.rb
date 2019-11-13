61947112d1a347fcc70682c009ae38e87c2bd7a5
JoshMcKinstry <jbmckinstry@gmail.com>
2019-11-12 18:06:12 -0700

    scaffold CarPart name:string

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :car_parts
   resources :cars
   resources :makes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
